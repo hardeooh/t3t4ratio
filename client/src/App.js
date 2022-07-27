cf40954c13f5a30d82b94fafab432e7771b7df55
Ashen-den <97136128+Ashen-den@users.noreply.github.com>
2022-07-27 03:04:54 -0700

    third Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import memories from './images/memories.png'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container maxwidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Memories
+          <img src={memories} alt="memories" height="60" />
+        </Typography>
+      </AppBar>
+    </Container>
   )
 }
 
